e5460c3c40dee63793b5fd3764a7a699e15385a1
Gabriel Mori <gabriel@DESKTOP-C3G6LIO>
2024-02-13 20:27:35 -0300

    Add Next.js, main page, and ignore files

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>New tab</title>
+      </Head>
+    </div>
+  );
+}
+
+export default Home;
